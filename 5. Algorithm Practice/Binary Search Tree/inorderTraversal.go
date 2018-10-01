b763a624aa6750a08f8c4577ca2a5b935c953409
Xiaojing <jojoxiaojing@icloud.com>
2018-09-30 18:36:32 -0700

    inorder, preorder

---
+++ 5. Algorithm Practice/Binary Search Tree/inorderTraversal.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+//TreeNode struct
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	var tn1, tn2, tn3 TreeNode
+	tn1.Val = 1
+	tn1.Left = &tn2
+	tn1.Right = &tn3
+	tn2.Val = 2
+	tn3.Val = 3
+
+	fmt.Println(inorderTraversal(&tn1))
+
+}
+
+func inorderTraversal(tn *TreeNode) []int {
+	result := []int{}
+
+	if tn == nil {
+		return result
+	}
+
+	if tn.Left != nil {
+		result = append(result, inorderTraversal(tn.Left)...)
+	}
+
+	result = append(result, tn.Val)
+
+	if tn.Right != nil {
+		result = append(result, inorderTraversal(tn.Right)...)
+	}
+
+	return result
+}
